49a237600c3427e0e121d51854926edcc668a82a
Wesley H. Gimenes <wehagy+github@gmail.com>
2023-08-27 04:20:20 -0300

    Initial commit

---
+++ .github/workflows/scripts/prepare-for-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+set -eux
+
+apk add --no-cache --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing \
+	distrobuilder
+
+apk add --no-cache \
+	bash \
+	git \
+	rsync \
+	tar
